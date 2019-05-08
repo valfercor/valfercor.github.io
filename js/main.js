d5f7da333c58a1c9ce44fd4b093c43257b67f072
Valeria Cortés <valeriafercd@gmail.com>
2019-05-08 14:08:13 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.carousel').carousel({
+	interval: 2000
+})
+
+$('#descripcion').scrollspy({ target: 'descripcion' })
+$('#nosotros').scrollspy({ target: 'nosotros' })
+$('#contacto').scrollspy({ target: 'contacto' })
+$('#inicio').scrollspy({ target: 'inicio' })
